0dbfed51130c63e6f3cf23b0df8828b28140eeb5
Smooth Pudding <dummy@address>
2025-12-25 20:05:34 +0900

    [feat] Define some types and interfaces

---
+++ src/types/game.ts
@@ -0,0 +1,26 @@
+type RollCount = 0 | 1 | 2 | 3;
+
+type GameMode = "play" | "analysis";
+
+interface GameState {
+  mode: GameMode;
+  rollCount: RollCount;
+  dice: number[];
+  scoreSheet: ScoreSheet;
+  lockedDice: boolean[];
+}
+
+interface ScoreSheet {
+  ace: number | null;
+  deuce: number | null;
+  trey: number | null;
+  four: number | null;
+  five: number | null;
+  six: number | null;
+  choice: number | null;
+  fourOfAKind: number | null;
+  fullHouse: number | null;
+  smallStraight: number | null;
+  bigStraight: number | null;
+  yacht: number | null;
+}
