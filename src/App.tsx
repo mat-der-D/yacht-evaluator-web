a249dfe3b7017b4e34dde1bc28f757ffb6eed6e7
Smooth Pudding <dummy@address>
2025-12-28 21:57:17 +0900

    [feat]

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Layout from './components/Layout';
 import { GameProvider } from './context/GameContext';
+import { useGame } from './context/GameContext';
+
+function AppContent() {
+  const { gameState } = useGame();
+
+  return (
+    <div className={`app app--${gameState.mode}`}>
+      <Layout />
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <GameProvider>
-      <Layout />
+      <AppContent />
     </GameProvider>
   );
 }
