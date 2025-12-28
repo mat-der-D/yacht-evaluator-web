ebf6bb929724ae653760cab32aaaae3e38172ff9
Smooth Pudding <dummy@address>
2025-12-28 19:47:46 +0900

    [feat] CSS for mode-tab

--- src/components/ModeTab.tsx
+++ src/components/ModeTab.tsx
@@ -4,15 +4,24 @@ const SELECTED_ICON = '●' as const;
 const UNSELECTED_ICON = '○' as const;
 
 export default function ModeTab() {
-  const { gameState, dispatch } = useGame();
+  const {
+    gameState: { mode },
+    dispatch,
+  } = useGame();
 
   return (
-    <div className="mode-tab">
-      <button onClick={() => dispatch({ type: 'CHANGE_MODE', payload: 'play' })}>
-        {gameState.mode === 'play' ? SELECTED_ICON : UNSELECTED_ICON} プレイ
+    <div className={`mode-tab mode-tab--${mode}`}>
+      <button
+        className={mode === 'play' ? 'active' : ''}
+        onClick={() => dispatch({ type: 'CHANGE_MODE', payload: 'play' })}
+      >
+        {mode === 'play' ? SELECTED_ICON : UNSELECTED_ICON} プレイ
       </button>
-      <button onClick={() => dispatch({ type: 'CHANGE_MODE', payload: 'analysis' })}>
-        {gameState.mode === 'analysis' ? SELECTED_ICON : UNSELECTED_ICON} 局面解析
+      <button
+        className={mode === 'analysis' ? 'active' : ''}
+        onClick={() => dispatch({ type: 'CHANGE_MODE', payload: 'analysis' })}
+      >
+        {mode === 'analysis' ? SELECTED_ICON : UNSELECTED_ICON} 局面解析
       </button>
     </div>
   );
